30861d14f76287b781018e6e21cb683d8e0e4a99
regend <kanecake@qq.com>
2014-01-15 15:46:11 +0800

    增加应用重启和清MC功能

--- LinuxScript/au.sh
+++ LinuxScript/au.sh
@@ -13,16 +13,17 @@ if [[ ! -d $1 ]];then
 do
  echo $i;
  file_name_s=$i;
+ done;
 	
 	cd /app/$1
 	rm -rf web.bak
 	cp -r web web.bak
-	cd web 
-		
-done;
-
+	cd /app/$1/web 
     cp -r $libdir/$1/$file_name_s ./
 	unzip -o $file_name_s
 	rm -rf $file_name_s
 	
+	./app/$1/resin-pro-3.1.8/bin/httpd_resin_autoprice5.sh restart
+	
+	cl 11211
 fi
